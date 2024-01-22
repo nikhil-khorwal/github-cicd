090ddaf93cf92086acd2c53055c7d50b87b70a6b
Nikhil Khorwal <nikhil.khorwal@zeronorth.com>
2024-01-22 11:42:10 +0530

    test

--- demo/main.py
+++ demo/main.py
@@ -1,7 +1,5 @@
 import logging
 
-from ddtrace import config, patch_all
-from ddtrace.contrib.asgi import TraceMiddleware
 from fastapi import FastAPI
 
 
@@ -9,6 +7,4 @@ log = logging.getLogger(__name__)
 log.level = logging.INFO
 
 
-
 app = FastAPI(docs_url="/docs", openapi_url="/openapi.json")
-
